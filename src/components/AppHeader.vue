e50d228e405104e00ea54e39cddec19ff42928b4
YoloSan <veselu@gmail.com>
2021-05-12 22:54:47 +0300

    add the first two tests

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <button v-show="loggedIn">Logout</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppHeader',
+  data() {
+    return {
+      loggedIn: false,
+    }
+  },
+}
+</script>
+
+<style scoped></style>
