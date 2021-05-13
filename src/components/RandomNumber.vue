7f27d080efcd26f8b500001bb88e82a312bc9f8c
YoloSan <veselu@gmail.com>
2021-05-13 09:26:54 +0300

    test props&user interaction

---
+++ src/components/RandomNumber.vue
@@ -0,0 +1,36 @@
+<template>
+  <span>{{ randomNumber }}</span>
+  <button @click="getRandomNumber">Generate Random Number</button>
+</template>
+
+<script>
+export default {
+  name: 'RandomNumber',
+
+  props: {
+    min: {
+      type: Number,
+      default: 1,
+    },
+    max: {
+      type: Number,
+      default: 10,
+    },
+  },
+
+  data() {
+    return {
+      randomNumber: 0,
+    }
+  },
+
+  methods: {
+    getRandomNumber() {
+      this.randomNumber =
+        Math.floor(Math.random() * (this.max - this.min + 1)) + this.min
+    },
+  },
+}
+</script>
+
+<style scoped></style>
